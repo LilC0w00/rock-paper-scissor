65619a7c262e195eab7a2e503de79ca92ccdaab0
Salma <dreambellytb@gmail.com>
2025-02-13 12:23:58 +0100

    Finalisation du script, essaie reussi, le projet est terminé

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const user = prompt("Choisissez Papier/Caillou/Ciseau").toLowerCase; // Demande a l'utilisateur d'entrée un des 3 choix meme avec majuscule
+const user = prompt("Choisissez Papier/Caillou/Ciseau").toLowerCase(); // Demande a l'utilisateur d'entrée un des 3 choix meme avec majuscule
 const robot = ["papier", "caillou", "ciseau"]; //tableau des 3 choix
 let choixHazard = Math.floor(Math.random() * robot.length); // un nombre random par rapport a la taille du tableau
 choixHazard = robot[choixHazard]; // par rapport a l'index recuperer la valeur
+
 console.log(`L'ordinateur a choisit : ${choixHazard}`);
-let userCaillou = user === Caillou;
+if (user === choixHazard) {
+  console.log("Il y'a égalité");
+} else if (
+  (user === "ciseau" && choixHazard === "papier") ||
+  (user === "papier" && choixHazard === "caillou") ||
+  (user === "caillou" && choixHazard === "ciseau")
+) {
+  console.log("Vous avez gagné!");
+} else if (
+  (user === "papier" && choixHazard === "ciseau") ||
+  (user === "caillou" && choixHazard === "papier") ||
+  (user === "ciseau" && choixHazard === "caillou")
+) {
+  console.log("Vous avez perdu...");
+}
